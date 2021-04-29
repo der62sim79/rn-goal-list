f82332a5859fe3927cdc6c4a7cece4685792b312
der62sim79 <ali.tonc@gmx.at>
2021-04-29 20:16:05 +0200

    input and output goals in a list

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, View, TextInput, Button, Text, ScrollView } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [yearGoals, setYearGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    // bei dieser schreibweise wird von rn garantiert eine vollständige liste zurück zu bekommen
+    setYearGoals((currentGoals) => [...yearGoals, enteredGoal]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.userInputRow}>
+        <TextInput
+          style={styles.userInput}
+          placeholder="Ziele 2021..."
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="Add" onPress={addGoalHandler} />
+      </View>
+      <ScrollView>
+        {yearGoals.map((goal) => (
+          <View style={styles.outputTextGoal} key={goal}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
+      <View></View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +42,24 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+  },
+  userInput: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    padding: 10,
+    width: "80%",
+  },
+  userInputRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  outputTextGoal: {
+    padding: 10,
+    backgroundColor: "#ccc",
+    borderColor: "pink",
+    borderWidth: 1,
+    marginVertical: 10,
   },
 });
