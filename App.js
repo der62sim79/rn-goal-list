d50e3706b8cee9b7b04908afc9b204900a7a8993
der62sim79 <ali.tonc@gmx.at>
2021-05-28 18:14:06 +0200

    First step basic

--- App.js
+++ App.js
@@ -1,31 +1,52 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  View,
-  FlatList,
-} from "react-native";
+import { StyleSheet, View, FlatList, Button } from "react-native";
 import GoalInput from "./components/GoalInput";
 import GoalItem from "./components/GoalItem";
 
 export default function App() {
   const [yearGoals, setYearGoals] = useState([]);
+  const [isAddMod, setIsAddMod] = useState(false);
 
-  const addGoalHandler = goalTitle => {
+  const addGoalHandler = (goalTitle) => {
     // bei dieser schreibweise wird von rn garantiert eine vollständige liste zurück zu bekommen
     // dem flatlist werden so objekte übergeben key lösung nicht perfekt aber reicht für die übung
     setYearGoals((currentGoals) => [
       ...yearGoals,
-      { key: Math.random().toString(), val: goalTitle },
+      { id: Math.random().toString(), val: goalTitle },
     ]);
+    setIsAddMod(false);
+  };
+
+  const removeGoalHandler = (goalId) => {
+    setYearGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  };
+
+  const cancelGoalAddHandler = () => {
+    setIsAddMod(false);
+    
   };
 
   return (
     <View style={styles.container}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title="Ziel Hinzufügen" onPress={() => setIsAddMod(true)} />
+      <GoalInput
+        visible={isAddMod}
+        onAddGoal={addGoalHandler}
+        onCancel={cancelGoalAddHandler}
+      />
       <FlatList
+        keyExtractor={(item, index) => item.id}
         data={yearGoals}
-        renderItem={(itemData) => <GoalItem title={itemData.item.val} />}
+        renderItem={(itemData) => (
+          <GoalItem
+            title={itemData.item.val}
+            onDelete={removeGoalHandler}
+            id={itemData.item.id}
+          />
+        )}
       />
 
       <View></View>
